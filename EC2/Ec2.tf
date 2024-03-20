8dc40caaebdc305bc07b762724e7a64c445d7c1e
shruraj111 <163994329+shruraj111@users.noreply.github.com>
2024-03-20 13:00:17 +0530

    Add files via upload

--- EC2/Ec2.tf
+++ EC2/Ec2.tf
@@ -1,1 +1,18 @@
+terraform {
+    required_providers {
+      aws = {
+        source = "registry.terraform.io/hashicorp/aws"
+        version = "~> 5.0"
+      }
+    }
+}
 
+provider "aws" {
+  region = "us-east-1"
+}
+  
+  resource "aws_instance" "ec2" {
+    ami = "ami-0d7a109bf30624c99"
+    instance_type = "t2.micro"
+    count = 2
+}
